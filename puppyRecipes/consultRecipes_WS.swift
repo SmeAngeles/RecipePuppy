2d1fe139c78584eb488e85949694e4d56f6213a6
Esmeralda Angeles <esmeralda.angeles@bancoazteca.com>
2021-07-16 10:51:34 -0500

    Mock en caso de fallar API
    En el caso de que la API este fallando, se leerá un archivo .json que contiene algunas recetas que se le pueden mostrar al usuario.

--- puppyRecipes/consultRecipes_WS.swift
+++ puppyRecipes/consultRecipes_WS.swift
@@ -17,17 +17,34 @@ public class consultRecipes_WS{
             let task = URLSession.shared.dataTask(with: request){ data, response, error in
                 if let error = error{
                     print("---> failed:", error.localizedDescription )
-                    return completion(.failure(error))
+                    guard let recipis = self.lookForRecipeLocal()else{return completion(.failure(error))}
+                    return completion(.success(recipis))
                 }
                 do {
                     let recipis = try JSONDecoder().decode([recepiDataModel].self, from: data!)
                     return completion(.success(recipis))
                 }
                 catch let jsonError{
-                    return completion(.failure(jsonError))
+                    guard let recipis = self.lookForRecipeLocal()else{return completion(.failure(jsonError))}
+                    return completion(.success(recipis))
                 }
             }
             task.resume()
         }
     }
+    
+    func lookForRecipeLocal()-> [recepiDataModel]?{
+        guard let  path = Bundle.main.path(forResource: "recipes", ofType: "json")  else { return nil }
+        do {
+            let data = try Data(contentsOf: URL(fileURLWithPath: path), options: .mappedIfSafe)
+            let recipis = try JSONDecoder().decode([recepiDataModel].self, from: data)
+            return recipis
+        } catch {
+            print("---> failed: error al leer el archivo json : ",error )
+            return nil
+        }
+    }
+    
+    
+    
 }
