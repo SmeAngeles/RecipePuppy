22e96a0535d27ab41601887b2e1d30dcebd331d9
Esmeralda Angeles <esmeralda.angeles@bancoazteca.com>
2021-07-16 16:34:50 -0500

    opción para mostrar recetas.
    En caso de no obtener una respuesta de la api se lee un archivo json local para mostrar algunas recetas

--- puppyRecipes/consultRecipes_WS.swift
+++ puppyRecipes/consultRecipes_WS.swift
@@ -13,38 +13,46 @@ public class consultRecipes_WS{
     
     func lookForRecipe(strKeyWord: String, completion: @escaping (Result<[recepiDataModel],Error >) -> ()){
         if let urlComplete = URL(string: "\(endpoint)\(strKeyWord)"){
+            
+            let configuration = URLSessionConfiguration.default
+            configuration.timeoutIntervalForRequest = 5
+            let session = URLSession(configuration: configuration)
             let request = URLRequest(url: urlComplete)
-            let task = URLSession.shared.dataTask(with: request){ data, response, error in
+            let task = session.dataTask(with: request){ data, response, error in
                 if let error = error{
                     print("---> failed:", error.localizedDescription )
-                    guard let recipis = self.lookForRecipeLocal()else{return completion(.failure(error))}
-                    return completion(.success(recipis))
+                    guard let recipis = self.lookForRecipeLocal(strKeyWord: strKeyWord)else{return completion(.failure(error))}
+                    DispatchQueue.main.sync{ return completion(.success(recipis))}
                 }
                 do {
-                    let recipis = try JSONDecoder().decode([recepiDataModel].self, from: data!)
-                    return completion(.success(recipis))
+                    guard let data = data  else{return}
+                    let recipis = try JSONDecoder().decode([recepiDataModel].self, from: data)
+                    DispatchQueue.main.sync{return completion(.success(recipis))}
                 }
                 catch let jsonError{
-                    guard let recipis = self.lookForRecipeLocal()else{return completion(.failure(jsonError))}
-                    return completion(.success(recipis))
+                    guard let recipis = self.lookForRecipeLocal(strKeyWord: strKeyWord)else{return completion(.failure(jsonError))}
+                    DispatchQueue.main.sync{ return completion(.success(recipis))}
                 }
             }
             task.resume()
         }
     }
     
-    func lookForRecipeLocal()-> [recepiDataModel]?{
+    func lookForRecipeLocal(strKeyWord: String)-> [recepiDataModel]?{
         guard let  path = Bundle.main.path(forResource: "recipes", ofType: "json")  else { return nil }
         do {
             let data = try Data(contentsOf: URL(fileURLWithPath: path), options: .mappedIfSafe)
-            let recipis = try JSONDecoder().decode([recepiDataModel].self, from: data)
-            return recipis
+            let listOfRecipes = try JSONDecoder().decode([recepiDataModel].self, from: data)
+            
+            let list = listOfRecipes.filter { (recipe) -> Bool in
+                recipe.mainIngredient == strKeyWord || recipe.ingredients.contains(strKeyWord)
+            }
+            return list
         } catch {
             print("---> failed: error al leer el archivo json : ",error )
             return nil
         }
     }
-    
     
     
 }
